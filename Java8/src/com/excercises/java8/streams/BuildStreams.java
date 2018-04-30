b4e41b6ea8407b81c08e29146d58ab76ef4f510d
HagosHaile <hagosb.haile@gmail.com>
2018-04-30 15:44:14 -0400

    streams

---
+++ Java8/src/com/excercises/java8/streams/BuildStreams.java
@@ -0,0 +1,27 @@
+package com.excercises.java8.streams;
+
+import java.util.stream.Stream;
+
+public class BuildStreams {
+	
+	     public static void main(String[] args){
+	    	
+	    	 System.out.println("\n=============================================================================");
+	    	//1) Using Stream.of(val1, val2, val3….)
+	         Stream<Integer> stream = Stream.of(1,2,3,4,5,6,7,8,9);
+	         stream.forEach(p -> System.out.println(p));
+	         
+	         System.out.println("\n=============================================================================");
+	         //2) Using Stream.of(arrayOfElements)
+	         
+	         System.out.println("\n=============================================================================");
+	         
+	         System.out.println("\n=============================================================================");
+	         
+	         System.out.println("\n=============================================================================");
+	         
+	         
+	         System.out.println("\n=============================================================================");
+	}
+
+}
